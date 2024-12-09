8c7eb9a3e0c1dd6da128be231c839009d787d261
dadothetato <dle32843@gmail.com>
2024-12-10 10:36:06 +1100

    Added CoursesGrid composable function to show all courses in a grid.

--- app/src/main/java/com/example/coursesapp/MainActivity.kt
+++ app/src/main/java/com/example/coursesapp/MainActivity.kt
@@ -7,21 +7,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.coursesapp.data.DataSource
 import com.example.coursesapp.model.Course
 import com.example.coursesapp.ui.theme.CoursesAppTheme
 
@@ -30,44 +38,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CoursesAppTheme {
-
-
-            }
-        }
-    }
-}
-
-@Composable
-fun CoursesCard(course: Course, modifier: Modifier = Modifier) {
-    Card(modifier = modifier) {
-        Row {
-            Image(
-                painterResource(course.coursePhotoImageResourceId),
-                contentDescription = stringResource(course.courseNameStringResourceId),
+            Surface(
                 modifier = Modifier
-                    .height(68.dp)
-                    .width(68.dp)
-            )
-            Column(
-                modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)
+                    .fillMaxSize()
+                    .statusBarsPadding(),
+                color = MaterialTheme.colorScheme.background
             ) {
-                Text(
-                    text = LocalContext.current.getString(course.courseNameStringResourceId),
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Row {
-                    Image(
-                        painterResource(R.drawable.grain),
-                        contentDescription = null,
-                        modifier = Modifier.size(16.dp)
-                    )
-                    Text(
-                        text = course.numberOfCourses.toString(),
-                        style = MaterialTheme.typography.labelMedium,
-                        modifier = Modifier.padding(start = 8.dp)
-                    )
+                CoursesAppTheme {
+                    CoursesGrid(DataSource.courses)
                 }
             }
         }
@@ -75,8 +53,77 @@ fun CoursesCard(course: Course, modifier: Modifier = Modifier) {
 }
 
 
+@Composable
+fun CoursesGrid(coursesList: List<Course>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2)
+    ) {
+        items(coursesList) { course ->
+            CoursesCard(
+                course = course,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_small)))
+
+        }
+    }
+}
+
+
+@Composable
+fun CoursesCard(course: Course, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Card(modifier = modifier) {
+            Row {
+                Image(
+                    painterResource(course.coursePhotoImageResourceId),
+                    contentDescription = stringResource(course.courseNameStringResourceId),
+                    modifier = Modifier
+                        .size(width = 68.dp, height = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+                Column(
+                    modifier = Modifier.padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        top = dimensionResource(R.dimen.padding_medium),
+                        end = dimensionResource(R.dimen.padding_medium))
+                ) {
+                    Text(
+                        text = LocalContext.current.getString(course.courseNameStringResourceId),
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontSize = 10.sp,
+                        modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_small))
+                    )
+                    Row {
+                        Image(
+                            painterResource(R.drawable.grain),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                        Text(
+                            text = course.numberOfCourses.toString(),
+                            style = MaterialTheme.typography.labelMedium,
+                            modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_small))
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+/*
 @Preview
 @Composable
 private fun CoursesCardPreview() {
     CoursesCard(Course(R.string.film, 123, R.drawable.film))
+}
+*/
+
+
+@Preview
+@Composable
+private fun CoursesGridPreview() {
+    CoursesGrid(DataSource.courses)
 }
