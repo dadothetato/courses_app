52071ecb4740fca7310beb2185b694c258c44e31
dadothetato <dle32843@gmail.com>
2024-12-10 09:52:11 +1100

    Renamed 'topic' to 'course'

--- app/src/main/java/com/example/coursesapp/model/Course.kt
+++ app/src/main/java/com/example/coursesapp/model/Course.kt
@@ -3,7 +3,7 @@ package com.example.coursesapp.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Topic(
+data class Course(
     @StringRes val courseNameStringResourceId: Int,
     val numberOfCourses: Int,
     @DrawableRes val coursePhotoImageResourceId: Int,
