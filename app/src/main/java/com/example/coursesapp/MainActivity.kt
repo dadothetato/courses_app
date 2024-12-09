9d9d160f5dd95e56e19518453176802ccccb743a
dadothetato <dle32843@gmail.com>
2024-12-10 10:11:27 +1100

    Added new composable function to show 1 course card.

--- app/src/main/java/com/example/coursesapp/MainActivity.kt
+++ app/src/main/java/com/example/coursesapp/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coursesapp.model.Course
 import com.example.coursesapp.ui.theme.CoursesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +31,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+
+            }
+        }
+    }
+}
+
+@Composable
+fun CoursesCard(course: Course, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Row {
+            Image(
+                painterResource(course.coursePhotoImageResourceId),
+                contentDescription = stringResource(course.courseNameStringResourceId),
+                modifier = Modifier
+                    .height(68.dp)
+                    .width(68.dp)
+            )
+            Column(
+                modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)
+            ) {
+                Text(
+                    text = LocalContext.current.getString(course.courseNameStringResourceId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Row {
+                    Image(
+                        painterResource(R.drawable.grain),
+                        contentDescription = null,
+                        modifier = Modifier.size(16.dp)
+                    )
+                    Text(
+                        text = course.numberOfCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = 8.dp)
                     )
                 }
             }
@@ -30,18 +74,9 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    CoursesAppTheme {
-        Greeting("Android")
-    }
+private fun CoursesCardPreview() {
+    CoursesCard(Course(R.string.film, 123, R.drawable.film))
 }
